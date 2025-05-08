556189cb185ed35f3ceb73571356459f566f8117
Junior Vargas <juniorvffc@gmail.com>
2025-05-08 16:08:37 -0300

    implementing user roles

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' #Caminho do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/flask-crud' #Caminho do banco de dados
 
 login_manager = LoginManager()
 db.init_app(app)
@@ -29,7 +30,7 @@ def login():
     # Login
     user = User.query.filter_by(username=username).first()
 
-    if user and user.password == password:
+    if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
       login_user(user)
       print(current_user.is_authenticated)
       return jsonify({"message": "Autenticação realizada com sucesso"})
@@ -49,7 +50,8 @@ def create_user():
   password = data.get("password")
 
   if username and password:
-    user = User(username=username, password=password)
+    hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+    user = User(username=username, password=hashed_password, role='user')
     db.session.add(user)
     db.session.commit()
     return jsonify({"message": "Usuario cadastrado com sucesso."})
@@ -72,6 +74,9 @@ def update_user(id_user):
   data = request.json
   user = User.query.get(id_user)
 
+  if id_user != current_user.id and current_user.role == "user":
+    return jsonify({"message": "Operação não permitida"}), 403
+  
   if user and data.get("password"):
     user.password = data.get("password")
     db.session.commit()
@@ -84,6 +89,8 @@ def update_user(id_user):
 def delete_user(id_user):
   user = User.query.get(id_user)
 
+  if current_user.role != 'admin':
+    return jsonify({"message": "Operação não permitida"}), 403
   if id_user == current_user.id:
     return jsonify({"message": "Deleção não permitida"}), 403
   
